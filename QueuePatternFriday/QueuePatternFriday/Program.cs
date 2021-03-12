ce40814f4d43bb055645aa4eaccf652ae9ac3604
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-03-12 14:49:40 +0200

    Add files via upload

---
+++ QueuePatternFriday/QueuePatternFriday/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueuePatternFriday
+{
+    class Program
+    {
+        static int counter = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string pwd = "1234";
+            string pwd_user = "";
+
+            do
+            {
+                Console.WriteLine("Enter password:");
+                pwd_user = Console.ReadLine();
+            } while (pwd_user != pwd);
+
+            Console.WriteLine("You get prize!");
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            WorkerQueue queue = new WorkerQueue(10);
+            for (int i = 0; i < 150000; i++)
+            {
+                int j = i;
+                queue.Produce(() => { Console.WriteLine($"new work {j}"); Thread.Sleep(1);
+                    Console.WriteLine("Completed...");
+                    counter++;
+                });
+            }
+
+            while (counter < 12)
+            {
+                Thread.Sleep(10);
+            }
+            sw.Stop();
+            Console.WriteLine($"Took {sw.Elapsed}");
+
+
+            Console.WriteLine("main completed...");
+        }
+    }
+}
---
+++ QueuePatternFriday/QueuePatternFriday/WorkerQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueuePatternFriday
+{
+    class WorkerQueue
+    {
+        //System.Collections.Concurrent.ConcurrentQueue<Action> m_queue = 
+        //new System.Collections.Concurrent.ConcurrentQueue<Action>();
+
+        private Queue<Action> m_queue = new Queue<Action>();
+
+        List<Thread> m_workers;
+
+        private object key = new object();
+
+        public int Count
+        {
+            get
+            {
+                return m_queue.Count;
+            }
+        }
+
+        public WorkerQueue(int num_workers)
+        {
+            m_workers = new List<Thread>(num_workers);
+            for (int i = 0; i < num_workers; i++)
+            {
+                Thread one_thread = new Thread(PendingConsume);
+                m_workers.Add(one_thread);
+                m_workers[i].Start();
+            }
+        }
+
+        private void PendingConsume()
+        {
+            while (true)
+            {
+                Action action = null;
+                while (m_queue.Count == 0)
+                {
+                    Thread.Sleep(10);
+                }
+                lock (key)
+                {
+                    //if (m_queue.TryDequeue(out Action action))
+                    //action.Invoke();
+                    if (m_queue.Count > 0)
+                    {
+                        action = m_queue.Dequeue();
+                    }
+                }
+                if (action != null)
+                {
+                    action.Invoke();
+                }
+            }
+        }
+
+        public void Produce(Action work)
+        {
+            try
+            {
+                Monitor.Enter(key);
+                // critical section
+
+                m_queue.Enqueue(work);
+            }
+            finally 
+            {
+                Monitor.Exit(key);
+            }
+        }
+    }
+}
